03ab3dfa78c3d6a69a1f1e9ae31d23bca1a68865
Dojo <dojo@jupiter.eclipse.ime.usp.br>
2008-09-08 20:58:53 -0300

    Jac saiu com teste para 3 elementos falhando. Mari&Jac

--- 50-haskell(2)/Sanduiche.hs
+++ 50-haskell(2)/Sanduiche.hs
@@ -1,5 +1,6 @@
 module Sanduiche where
 
-montaMatriz [1,2] = [[1,2],[2,4]] 
 
+montaMatriz [1,2,3] = [[1,2,3],[2,4,6],[3,6,9]]
+montaMatriz [x,y] = [[x*x,x*y],[x*y,y*y]] 
 montaMatriz [x] = [[x*x]]
--- 50-haskell(2)/Test.hs
+++ 50-haskell(2)/Test.hs
@@ -7,11 +7,16 @@ main = runTestTT testes
 
 testes = TestList [verificaMontaMatriz]
 
-verificaMontaMatriz = TestList ["Nao monta uma matriz de tamanho 1" ~:
-								[[1]] ~=? montaMatriz [1],
-								"Nao monta uma matriz de tamanho 1 com valor 2" ~:
-								[[4]] ~=? montaMatriz [2],
-								"Nao monta uma matriz de tamanho 2" ~:
-								[[1,2],[2,4]] ~=? montaMatriz [1,2]
-								]
+verificaMontaMatriz = TestList
+	["Nao monta uma matriz de tamanho 1" ~:
+	[[1]] ~=? montaMatriz [1]
+	,"Nao monta uma matriz de tamanho 1 com valor 2" ~:
+	[[4]] ~=? montaMatriz [2]
+	,"Nao monta uma matriz de tamanho 2" ~:
+	[[1,2],[2,4]] ~=? montaMatriz [1,2]
+	,"Nao monta uma matriz de tamanho 2 com valores certos" ~:
+	[[100,200],[200,400]] ~=? montaMatriz [10,20]
+	,"Nao monta uma matriz de tamanho 3 com valores certos" ~:
+	[[1,2,3],[2,4,6],[3,6,9]] ~=? montaMatriz [1,2,3]
+	]
 

f0fd4c9a54a45cd8ff4226f204e9d890553635da
Dojo@SP <dojo@logiciel.(none)>
2008-09-22 21:47:47 +0200

    Converte para 2 com funcao bizarra. Breno e Hugo.

--- 52-setun/Trit.hs
+++ 52-setun/Trit.hs
@@ -3,7 +3,11 @@ module Trit where
 converte 0 = "0"
 converte 1 = "+"
 converte (-1) = "-"
-converte 2 = "+-"
+converte x =
+    let
+        pot = maxPot3 (x+1)
+    in
+      "+" ++ converte(x-pot)
 
 maxPot3 :: Int -> Int 
 maxPot3 x

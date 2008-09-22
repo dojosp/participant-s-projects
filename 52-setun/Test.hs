cda8fd6636d829887915dc8b84b5927f83f7fe91
Dojo@SP <dojo@logiciel.(none)>
2008-09-22 21:21:00 +0200

    Passando maxPot3. Hugo e R

--- 52-setun/Test.hs
+++ 52-setun/Test.hs
@@ -20,7 +20,10 @@ testeConverteParaUmDigito =
 testeMaiorPotenciaQueCabe =
     TestList
     [
-     "Maior potencia de 3 que cabe em 1 e' 0" ~:
+     "Maior potencia de 3 que cabe em 0 e' 0" ~:
+     0  ~=? maxPot3 0
+
+     ,"Maior potencia de 3 que cabe em 1 e' 0" ~:
      0  ~=? maxPot3 1
 
      ,"Maior potencia de 3 que cabe em 2 e' 0" ~:
@@ -32,8 +35,11 @@ testeMaiorPotenciaQueCabe =
      ,"Maior potencia de 3 que cabe em 4 e' 1" ~:
      1  ~=? maxPot3 4
 
+     ,"Maior potencia de 3 que cabe em 9 e' 2" ~:
+     2  ~=? maxPot3 9
+
 --     ,"Maior potencia de 3 que cabe em 9 e' 1" ~:
-  --   1  ~=? maxPot3 4
+--     1  ~=? maxPot3 4
 
 
     ]
--- 52-setun/Trit.hs
+++ 52-setun/Trit.hs
@@ -3,11 +3,10 @@ module Trit where
 converte 0 = "0"
 converte 1 = "+"
 
---maxPot3 4 = 1
---maxPot3 3 = 1
 maxPot3 :: Int -> Int 
-maxPot3 x = x `div` 3
-
+maxPot3 x
+    | x < 3 = 0
+    | otherwise = 1 + maxPot3 (x `div` 3)
 
 
 

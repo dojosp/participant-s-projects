6f766ed35126d067285648ca699e4c23cdcf69f9
Dojo@SP <dojo@logiciel.(none)>
2008-09-22 21:31:58 +0200

    Mudamos a semantica do maxPot3 para indicar a potencia propriamente dita (nao o expoente). Todos os testes falhando. rbp & Mari.

--- 52-setun/Test.hs
+++ 52-setun/Test.hs
@@ -20,26 +20,22 @@ testeConverteParaUmDigito =
 testeMaiorPotenciaQueCabe =
     TestList
     [
-     "Maior potencia de 3 que cabe em 0 e' 0" ~:
-     0  ~=? maxPot3 0
+     "Maior potencia de 3 que cabe em 0 e' 1" ~:
+     1  ~=? maxPot3 0
 
-     ,"Maior potencia de 3 que cabe em 1 e' 0" ~:
-     0  ~=? maxPot3 1
+     ,"Maior potencia de 3 que cabe em 1 e' 1" ~:
+     1  ~=? maxPot3 1
 
-     ,"Maior potencia de 3 que cabe em 2 e' 0" ~:
-     0  ~=? maxPot3 2
+     ,"Maior potencia de 3 que cabe em 2 e' 1" ~:
+     1  ~=? maxPot3 2
 
-     ,"Maior potencia de 3 que cabe em 3 e' 1" ~:
-     1  ~=? maxPot3 3
+     ,"Maior potencia de 3 que cabe em 3 e' 3" ~:
+     3  ~=? maxPot3 3
 
-     ,"Maior potencia de 3 que cabe em 4 e' 1" ~:
-     1  ~=? maxPot3 4
+     ,"Maior potencia de 3 que cabe em 4 e' 3" ~:
+     3  ~=? maxPot3 4
 
-     ,"Maior potencia de 3 que cabe em 9 e' 2" ~:
-     2  ~=? maxPot3 9
-
---     ,"Maior potencia de 3 que cabe em 9 e' 1" ~:
---     1  ~=? maxPot3 4
-
+     ,"Maior potencia de 3 que cabe em 9 e' 9" ~:
+     9  ~=? maxPot3 9
 
     ]

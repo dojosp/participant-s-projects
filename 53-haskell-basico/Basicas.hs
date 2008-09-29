935732d5aab44f403ecdd275384a8015b023a547
Dojo@SP <dojo@logiciel.(none)>
2008-09-29 21:26:39 +0200

    Passando com refatoracoes. Breno e Mari.

--- 53-haskell-basico/Basicas.hs
+++ 53-haskell-basico/Basicas.hs
@@ -1,7 +1,6 @@
 module Basicas where
 
 mapeia :: [a] -> (a -> b) -> [b]
-mapeia [a] funcao = [(funcao a)]
-mapeia [a , b] funcao = [(funcao a) , (funcao b)]
+mapeia (a : resto) funcao = (funcao a):mapeia resto funcao
 mapeia [] funcao = [] 
 

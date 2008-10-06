e6b11e363b34fd78a13204d4a4ba06b981114dd2
ticolucci <ticolucci@gmail.com>
2008-10-06 22:09:52 +0200

    Refatorando para tentar tratar o EOF mas sem sucesso. Hugo & Thiago

--- 54-stdin-haskell/QuebrandoTDD.hs
+++ 54-stdin-haskell/QuebrandoTDD.hs
@@ -6,6 +6,9 @@ import IO
 main = do
   hSetBuffering stdin LineBuffering
   linha <- getLine
+  printSolucao linha
+
+printSolucao linha = do
   putStr (linha ++ " ")
   putStrLn (show ((maiorCicloEntre' linha) + 1))
   main

23f38b787e179805dc973eab10c17b3298d4c9d5
ticolucci <ticolucci@gmail.com>
2008-10-06 20:55:20 +0200

    Proximo de 3 eh 10 - quick fix. Mari e Jac

--- 54-stdin-haskell/Testes.hs
+++ 54-stdin-haskell/Testes.hs
@@ -14,4 +14,6 @@ testeProximoNumero =
      proximo 2 ~?= 1
     ,"Proximo numero depois 4 deveria ser 2" ~:
      proximo 4 ~?= 2
+    ,"Proximo numero depois 3 deveria ser 10" ~:
+     proximo 3 ~?= 10
     ]
--- 54-stdin-haskell/TresNmaisUm.hs
+++ 54-stdin-haskell/TresNmaisUm.hs
@@ -1,4 +1,5 @@
 module TresNmaisUm where
 
 proximo :: Int -> Int
+proximo 3 = 10
 proximo x = x `div` 2

3f02836010920067265a48717db2a68239372697
ticolucci <ticolucci@gmail.com>
2008-10-06 21:24:35 +0200

    Refatorando. Thiago XDeou Hugo e Thiago

--- 54-stdin-haskell/TresNmaisUm.hs
+++ 54-stdin-haskell/TresNmaisUm.hs
@@ -7,5 +7,5 @@ proximo x | even x = x `div` 2
 ciclo 1 = 0
 ciclo n = 1 + ciclo (proximo n)
 
-maiorCicloEntre i j | i < j = j-1
-                    | otherwise = i-1
+maiorCicloEntre i j | i <= j = j-1
+                    | otherwise = maiorCicloEntre j i

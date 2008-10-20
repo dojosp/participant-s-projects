50f46c7ae56812eb22db72dbb4b78b6ab677817b
Fabricio Nascimento <fabriciosn@gmail.com>
2008-10-20 21:29:23 -0200

    Refatorando o terceiro termo
    Ramon & Thiago

--- 55-ruby/lib/count_and_say.rb
+++ 55-ruby/lib/count_and_say.rb
@@ -12,7 +12,9 @@ class CountAndSay
     elsif index == 2
       ('1' + self[1].to_s).to_i
     elsif index == 3
-      21
+      second_term = self[2].to_s
+      second_term[0,1] = '2'
+      second_term.to_i
     else
       1211
     end

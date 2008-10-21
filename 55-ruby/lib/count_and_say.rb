6652458b9511ec07d52e8b40497809ffa094eac5
Fabricio Nascimento <fabriciosn@gmail.com>
2008-10-20 22:16:38 -0200

    Refatorando o []
    Breno e RAFAEL

--- 55-ruby/lib/count_and_say.rb
+++ 55-ruby/lib/count_and_say.rb
@@ -12,8 +12,11 @@ class CountAndSay
     elsif index <= 3
       (number_of(@first.to_s, self[index-1].to_s).to_s + '1').to_i
     else
-      third_term = self[3].to_s
-      fourth_term = "1" + third_term + "1"
+    
+      number_of_twos = number_of("2", self[index-1].to_s).to_s
+      number_of_ones = number_of("1", self[index-1].to_s).to_s  
+      fourth_term = number_of_twos + "2" + number_of_ones + "1"
+      
       fourth_term.to_i
     end
   end

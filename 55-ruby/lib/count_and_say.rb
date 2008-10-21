5fc803b3ad9e17dc9af9ebaa6aa9afe4b4511183
Fabricio Nascimento <fabriciosn@gmail.com>
2008-10-20 22:04:00 -0200

    Refatorando classe CountAndSay
    Andre e Paulo

--- 55-ruby/lib/count_and_say.rb
+++ 55-ruby/lib/count_and_say.rb
@@ -9,12 +9,8 @@ class CountAndSay
   def [](index)
     if index == 1
       @first
-    elsif index == 2
+    elsif index <= 3
       (number_of(@first.to_s, self[index-1].to_s).to_s + '1').to_i
-    elsif index == 3
-      second_term = self[2].to_s
-      second_term[0,1] = '2'
-      second_term.to_i
     else
       third_term = self[3].to_s
       fourth_term = "1" + third_term + "1"

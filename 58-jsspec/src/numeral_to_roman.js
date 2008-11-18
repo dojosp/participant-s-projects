5d625900e1084531fd472bcbed3a77811bd22f3c
Hugo Corbucci <hugo.corbucci@gmail.com>
2008-11-18 00:41:40 -0200

    Usando o mapa apenas na base. hugo

--- 58-jsspec/src/numeral_to_roman.js
+++ 58-jsspec/src/numeral_to_roman.js
@@ -2,6 +2,7 @@ mapa = {0:"", 1:"I", 5:"V"}
 
 Number.prototype.to_roman = function() {
     if(this in mapa) return mapa[this]
-    if((this+1) in mapa) return mapa[1] + (this+1).to_roman()
-    return (this-1).to_roman() + 1.to_roman()
+    var num = 1
+    if((this+num) in mapa) return num.to_roman() + (this+num).to_roman()
+    return (this-num).to_roman() + num.to_roman()
 }

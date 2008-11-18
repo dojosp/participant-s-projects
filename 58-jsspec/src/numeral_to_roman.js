55f501fc7f2b31469254904b91a889adf5765f8d
Hugo Corbucci <hugo.corbucci@gmail.com>
2008-11-18 00:48:25 -0200

    Tornando o menor_que dinâmico. Hugo

--- 58-jsspec/src/numeral_to_roman.js
+++ 58-jsspec/src/numeral_to_roman.js
@@ -1,4 +1,5 @@
 mapa = {0:"", 1:"I", 5:"V"}
+lista = [0, 1, 5]
 
 Number.prototype.to_roman = function() {
     if(this in mapa) return mapa[this]
@@ -8,5 +9,6 @@ Number.prototype.to_roman = function() {
 }
 
 function menor_que(num, mapa) {
-    return 1
+    for(var i = 0; lista[i] < num; i++);
+    return lista[i-1]
 }

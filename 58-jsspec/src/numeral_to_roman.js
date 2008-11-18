5b5961a73427bd4c7ddb4541eed0ebc82ce3ef84
Hugo Corbucci <hugo.corbucci@gmail.com>
2008-11-18 00:36:45 -0200

    Passando com mapa com 1 hard coded. Hugo

--- 58-jsspec/src/numeral_to_roman.js
+++ 58-jsspec/src/numeral_to_roman.js
@@ -2,5 +2,6 @@ mapa = {0:"", 1:"I", 5:"V"}
 
 Number.prototype.to_roman = function() {
     if(this in mapa) return mapa[this]
+    if((this+1) in mapa) return mapa[1] + (this+1).to_roman()
     return "I" + (this-1).to_roman()
 }

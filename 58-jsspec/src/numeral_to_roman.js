adb4ae69b6b0b7d4ab230d9481f0b67bbc1a3e72
Hugo Corbucci <hugo.corbucci@gmail.com>
2008-11-18 00:43:26 -0200

    Introduzindo uma função menor_que que deve achar o maior número no mapa menor que o pedido. Hugo

--- 58-jsspec/src/numeral_to_roman.js
+++ 58-jsspec/src/numeral_to_roman.js
@@ -2,7 +2,11 @@ mapa = {0:"", 1:"I", 5:"V"}
 
 Number.prototype.to_roman = function() {
     if(this in mapa) return mapa[this]
-    var num = 1
+    var num = menor_que(this, mapa)
     if((this+num) in mapa) return num.to_roman() + (this+num).to_roman()
     return (this-num).to_roman() + num.to_roman()
+}
+
+function menor_que(num, mapa) {
+    return 1
 }

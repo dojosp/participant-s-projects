d673b23998911ffb4103131737c234fcc078bc4a
Hugo Corbucci <hugo.corbucci@gmail.com>
2008-11-18 00:28:55 -0200

    Novo teste para tirar a roubalheira. Hugo

--- 58-jsspec/test/unit/numeral_to_roman_spec.js
+++ 58-jsspec/test/unit/numeral_to_roman_spec.js
@@ -5,4 +5,7 @@ describe("Arabic number", {
     "should convert 1 to 'I'": function() {
         value_of((1).to_roman()).should_be("I")
     },
+    "should convert 2 to 'II'": function() {
+        value_of((2).to_roman()).should_be("II")
+    },
 })

a9107ddaef213e70d1ba5561d81a13eed6f04e7b
Hugo Corbucci <hugo.corbucci@gmail.com>
2008-11-18 00:49:30 -0200

    Passando agora para 6 reinvertendo o else. Hugo

--- 58-jsspec/src/numeral_to_roman.js
+++ 58-jsspec/src/numeral_to_roman.js
@@ -5,7 +5,7 @@ Number.prototype.to_roman = function() {
     if(this in mapa) return mapa[this]
     var num = menor_que(this, mapa)
     if((this+num) in mapa) return num.to_roman() + (this+num).to_roman()
-    return (this-num).to_roman() + num.to_roman()
+    return num.to_roman() + (this-num).to_roman()
 }
 
 function menor_que(num, mapa) {

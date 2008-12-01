2937d772bf7da67b90a9bb9fd2a5a5a8d4b87f48
ticolucci <ticolucci@gmail.com>
2008-12-01 20:42:56 +0200

    Teste com uma caixa roubado Pedro e Thiago

--- 59-pile-of-boxes/PileBoxes.hs
+++ 59-pile-of-boxes/PileBoxes.hs
@@ -1,7 +1,8 @@
 module PileBoxes where
 
 data Pilha = Pilha [Box]
-data Box = Box
+data Box = Box (Int,Int)
 
 tamanho :: Pilha -> Int
+tamanho (Pilha [Box (1,1)]) = 1
 tamanho _ = 0
--- 59-pile-of-boxes/Tests.hs
+++ 59-pile-of-boxes/Tests.hs
@@ -5,9 +5,14 @@ import PileBoxes
 
 main = runTestTT testes
 
-testes = TestList[testePilhaVazia]
+testes = TestList[testePilhaVazia, testeUmaCaixa]
 
 testePilhaVazia = TestList
 	["Nenhuma caixa deveria ter tamanho 0" ~:
 	tamanho (Pilha []) ~?= 0
+	]
+
+testeUmaCaixa = TestList
+	["Pilha de uma caixa de tamanho 1 deveria ter tamanho 1" ~:
+	tamanho (Pilha [Box (1,1)]) ~?= 1
 	]

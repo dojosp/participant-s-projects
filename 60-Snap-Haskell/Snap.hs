6a9cdeb31c1d48ae14a216cd5bc48cd82ffbf4ac
ticolucci <ticolucci@gmail.com>
2008-12-08 21:11:41 +0200

    Passando os testes implementando pitágoras Pedro e Bani

--- 60-Snap-Haskell/Snap.hs
+++ 60-Snap-Haskell/Snap.hs
@@ -1,8 +1,8 @@
 module Snap where
 
-data Ponto = Ponto Int Int
+data Ponto = Ponto Float Float
 
 
 
-distancia :: Ponto -> Ponto -> Int
-distancia (Ponto x1 y1) (Ponto x2 y2 ) = x2-x1 + y2-y1
+distancia :: Ponto -> Ponto -> Float
+distancia (Ponto x1 y1) (Ponto x2 y2 ) = sqrt ((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
--- 60-Snap-Haskell/Tests.hs
+++ 60-Snap-Haskell/Tests.hs
@@ -9,13 +9,15 @@ testes = TestList [testeCalculaDistancias]
 
 testeCalculaDistancias = TestList[
 	"Distancia entre um ponto e ele mesmo deveria ser 0" ~:
-	distancia (Ponto 0 0) (Ponto 0 0 ) ~?= 0
+	distancia (Ponto 0 0) (Ponto 0 0 ) ~?= 0.0
 	,"Distancia entre o ponto 0 0 e 0 1 deveria ser 1" ~:
-	distancia (Ponto 0 0) (Ponto 0 1 ) ~?= 1
+	distancia (Ponto 0 0) (Ponto 0 1 ) ~?= 1.0
 	,"Distancia entre o ponto 0 0 e 0 2 deveria ser 2" ~:
-	distancia (Ponto 0 0) (Ponto 0 2 ) ~?= 2
+	distancia (Ponto 0 0) (Ponto 0 2 ) ~?= 2.0
 	,"Distancia entre o ponto 0 0 e 1 0 deveria ser 1" ~:
-	distancia (Ponto 0 0) (Ponto 1 0 ) ~?= 1
+	distancia (Ponto 0 0) (Ponto 1 0 ) ~?= 1.0
 	,"Distancia entre o ponto 1 0 e 1 1 deveria ser 1" ~:
-	distancia (Ponto 1 0) (Ponto 1 1 ) ~?= 1
+	distancia (Ponto 1 0) (Ponto 1 1 ) ~?= 1.0
+	,"Distancia entre o ponto 0 3 e 4 0 deveria ser 5" ~:
+	distancia (Ponto 0 3) (Ponto 4 0 ) ~?= 5
 	]

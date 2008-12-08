9f9cf2712eb3160d1c30cbac93a02a0b6ae5b036
ticolucci <ticolucci@gmail.com>
2008-12-08 21:33:40 +0200

    Refatorando Breno e Hugo

--- 60-Snap-Haskell/Snap.hs
+++ 60-Snap-Haskell/Snap.hs
@@ -4,9 +4,7 @@ data Ponto = Ponto Float Float
 data PontoAtrator = PontoAtrator Ponto Float
 
 pertenceARegiao :: Ponto -> PontoAtrator -> Bool
-pertenceARegiao (Ponto x _) (PontoAtrator _ raio)
-	| raio < x  =  False
-pertenceARegiao _ _ = True
+pertenceARegiao (Ponto x _) (PontoAtrator _ raio) = raio >= x
 
 distancia :: Ponto -> Ponto -> Float
 distancia (Ponto x1 y1) (Ponto x2 y2 ) = sqrt ((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))

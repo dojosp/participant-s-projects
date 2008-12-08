7dcb3f687602fc4ef6452042c15360b9b0c9b325
ticolucci <ticolucci@gmail.com>
2008-12-08 20:50:48 +0200

    passando sem roubadinha Juca e Ricardo

--- 60-Snap-Haskell/Snap.hs
+++ 60-Snap-Haskell/Snap.hs
@@ -5,6 +5,4 @@ data Ponto = Ponto Int Int
 
 
 distancia :: Ponto -> Ponto -> Int
-distancia (Ponto 0 0) (Ponto 0 1 ) = 1
-distancia (Ponto 0 0) (Ponto 0 2 ) = 2
-distancia _ _ = 0
+distancia (Ponto 0 a) (Ponto 0 b ) = b-a

61ae5a1f3e0c075c93f3dcc24bd5d3cd64d6f94d
Thiago Rodrigues Colucci <ticolucci@marvin.(none)>
2009-03-09 21:31:40 -0300

    Usando vizinhos no dfs

--- 67-Energy/Energy.hs
+++ 67-Energy/Energy.hs
@@ -8,8 +8,6 @@ ehConexo _ = True
 
 dfs :: Grafo -> [Int]
 dfs (Grafo 0 _) = []
-dfs (Grafo _ []) = [1]
-dfs (Grafo 2 _) = [1, 2]
 dfs (Grafo _ arestas) = 1:(vizinhos 1 arestas)
 
 vizinhos :: Int -> [(Int, Int)] -> [Int]

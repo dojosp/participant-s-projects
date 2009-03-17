fb15b4a966a9f6722d5cb8c062098508aea4c0f9
Thiago Rodrigues Colucci <ticolucci@marvin.(none)>
2009-03-16 21:27:39 -0300

    Depois de muito apanhar, mudamos tudo pra float e está passando os testes. Mari e Hugo

--- 68-FlocosDeNeve/Flocos.hs
+++ 68-FlocosDeNeve/Flocos.hs
@@ -1,4 +1,14 @@
 module Flocos where
 
-calculaPontas::(Int,Int) -> Int -> [(Int,Int)]
-calculaPontas _ _ = [(0,10),(9, 5),(9, -5)]
+type Raio = Float
+type Ponto = (Float,Float)
+
+calculaPontas :: Ponto -> Raio -> [Ponto]
+calculaPontas _ raio = [(0, raio),
+                        (coseno30*raio, raio*seno30),
+                        (raio*coseno30, -raio*seno30)]
+
+seno30 = 0.5
+coseno30 :: Float
+coseno30 = (sqrt 3 ) / 2
+           
--- 68-FlocosDeNeve/TesteFlocos.hs
+++ 68-FlocosDeNeve/TesteFlocos.hs
@@ -14,7 +14,7 @@ testeCalculaPontas =
      "Calcula primeira ponta do floco em 0,0 com raio 10" ~:
      head (calculaPontas (0,0) 10) ~?= (0,10)
     ,"Calcula segunda ponta do floco em 0,0 com raio 10" ~:
-     head (tail (calculaPontas (0,0) 10)) ~?= (9, 5)
+     head (tail (calculaPontas (0,0) 10)) ~?= (5 * (sqrt 3), 5)
     ,"Calcula terceira ponta do floco em 0,0 com raio 10" ~:
-     head (tail (tail (calculaPontas (0,0) 10))) ~?= (9, -5)
+     head (tail (tail (calculaPontas (0,0) 10))) ~?= (5 * (sqrt 3), -5)
     ]

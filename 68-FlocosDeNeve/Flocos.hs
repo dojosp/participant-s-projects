f8abf8a9aacccd8620ee9836ba77947339844e14
Thiago Rodrigues Colucci <ticolucci@marvin.(none)>
2009-03-16 21:35:31 -0300

    Passando testes considerando o centro. hugo e jac

--- 68-FlocosDeNeve/Flocos.hs
+++ 68-FlocosDeNeve/Flocos.hs
@@ -4,12 +4,12 @@ type Raio = Float
 type Ponto = (Float,Float)
 
 calculaPontas :: Ponto -> Raio -> [Ponto]
-calculaPontas _ raio = [(0, raio),
-                        (coseno30*raio, raio*seno30),
-                        (raio*coseno30, -raio*seno30),
-                        (0, -raio),
-                        (-coseno30*raio, -raio*seno30),
-                        (-raio*coseno30, raio*seno30)]
+calculaPontas (x,y) raio = [(x, y+raio),
+                            (x+coseno30*raio, y+raio*seno30),
+                            (x+raio*coseno30, y-raio*seno30),
+                            (x, y-raio),
+                            (x-coseno30*raio, y-raio*seno30),
+                            (x-raio*coseno30, y+raio*seno30)]
 
 seno30 = 0.5
 coseno30 :: Float
--- 68-FlocosDeNeve/TesteFlocos.hs
+++ 68-FlocosDeNeve/TesteFlocos.hs
@@ -19,4 +19,11 @@ testeCalculaPontas =
      head (tail (tail (calculaPontas (0,0) 10))) ~?= (5 * (sqrt 3), -5)
     ,"Calcula as 3 últimas pontas do floco em 0,0 com raio 10" ~:
      tail (tail (tail (calculaPontas (0,0) 10))) ~?= [(0, -10),(-5 * (sqrt 3), -5), (-5* (sqrt 3), 5)]
+    ,"Calcula todas as pontas do floco em 1,1 com raio 10" ~:
+     calculaPontas (1,1) 10 ~?= [(1,11),
+                                 (1+5*(sqrt 3), 6),
+                                 (1+5*(sqrt 3), -4),
+                                 (1, -9),
+                                 (1-5*(sqrt 3), -4),
+                                 (1-5*(sqrt 3), 6)]
     ]

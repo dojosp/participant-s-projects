51dc573196f34dd3f08b34d2a40dd71c9bb9ce5c
Thiago Rodrigues Colucci <ticolucci@marvin.(none)>
2009-04-01 11:03:27 -0300

    ajustando repositorio para dar push

--- 69-Vizinhos/lib/vizinhos.rb
+++ 69-Vizinhos/lib/vizinhos.rb
@@ -1,22 +1,42 @@
 class Vizinhanca
 
-  attr_accessor :tamanho_da_vizinhanca
+  attr_accessor :tamanho_da_vizinhanca, :lista_de_vizinhos
 
   def initialize tamanho_da_vizinhanca
     @tamanho_da_vizinhanca = tamanho_da_vizinhanca
+    @lista_de_vizinhos = []
+    @tamanho_da_vizinhanca.times do
+      @lista_de_vizinhos<<[]
+    end
   end
 
   def adiciona morador , vizinho
-
+    unless morador == vizinho
+      @lista_de_vizinhos[morador] << vizinho
+    end
   end
 
   def vizinhos
-    if @tamanho_da_vizinhanca == 2
-      [ [1] ]
-    else
-      [[1,2],[0,2],[]]
-    end
-
+    @lista_de_vizinhos
   end
 
+  def melhor_lugar
+    lista_de_tamanhos=[]
+    @lista_de_vizinhos.each do |lista_do_morador|
+      lista_de_tamanhos << lista_do_morador.size
+    end
+    menor = @tamanho_da_vizinhanca-1
+    lista_de_tamanhos.each do |numero_do_vizinhos|
+      if numero_do_vizinhos < menor
+        menor = numero_do_vizinhos
+      end
+    end
+    melhores_lugares=[]
+    lista_de_tamanhos.each_with_index do |v, morador|
+      if v == menor
+        melhores_lugares << morador
+      end
+    end
+    melhores_lugares
+  end
 end
--- 69-Vizinhos/spec/vizinhos_spec.rb
+++ 69-Vizinhos/spec/vizinhos_spec.rb
@@ -26,4 +26,32 @@ describe "Vizinhanca" do
     vizinhanca.vizinhos[0].should == [1,2]
   end
 
+  it "nao deveria si propria como vizinho" do
+    vizinhanca = Vizinhanca.new 2
+    vizinhanca.adiciona 0, 0
+    vizinhanca.adiciona 0, 1
+    vizinhanca.vizinhos[0].size.should == 1
+    vizinhanca.vizinhos[0].should == [1]
+  end
+
+  it "deveria retornar o morador 1 quando lista de vizinhos eh [[1,2],[2],[0,1]]" do
+    vizinhanca = Vizinhanca.new 3
+    vizinhanca.adiciona 0,1
+    vizinhanca.adiciona 0,2
+    vizinhanca.adiciona 1,2
+    vizinhanca.adiciona 2,0
+    vizinhanca.adiciona 2,1
+    vizinhanca.melhor_lugar.should == [1]
+  end
+
+  it "deveria retornar o morador 2 quando lista de vizinhos eh [[1,2],[0,2],[0]]" do
+    vizinhanca = Vizinhanca.new 3
+    vizinhanca.adiciona 0, 1
+    vizinhanca.adiciona 0, 2
+    vizinhanca.adiciona 1, 2
+    vizinhanca.adiciona 1, 0
+    vizinhanca.adiciona 2, 0
+    vizinhanca.melhor_lugar.should == [2]
+  end
+
 end

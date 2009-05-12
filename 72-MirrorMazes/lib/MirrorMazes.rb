beea6ab73e703ff78987d0e3d256c86ac0207dda
Thiago Rodrigues Colucci <ticolucci@marvin.(none)>
2009-05-11 21:06:48 -0300

    Entrada deu certo Mari & Joao

--- 72-MirrorMazes/lib/MirrorMazes.rb
+++ 72-MirrorMazes/lib/MirrorMazes.rb
@@ -1,13 +1,20 @@
 class MirrorMaze
   def initialize mapa
-
+    @mapa = mapa.split
   end
 
   def entrada
-    [0,0]
+    @mapa.each_with_index do |linha, indice|
+      indice_caracter = 0
+      linha.each_char do |caracter|
+        return [indice, indice_caracter] if caracter == '.'
+        indice_caracter += 1
+      end
+    end
+    [0,1]
   end
 
   def saída
-    [0,2]
+    [0,1]
   end
 end
--- 72-MirrorMazes/spec/MirrorMazes_spec.rb
+++ 72-MirrorMazes/spec/MirrorMazes_spec.rb
@@ -4,15 +4,23 @@ require 'lib/MirrorMazes'
 
 describe "MirrorMazes" do
   it "deveria criar um labirinto" do
-    maze = MirrorMaze.new "***\n
-                           ...\n
-                           ***\n"
+    maze = MirrorMaze.new "***
+                           ...
+                           ***"
     maze.should_not be_nil
   end
 
   it "deveria identificar entrada e saída" do
-    maze = MirrorMaze.new "...\n"
-    maze.entrada.should == [0,0]
-    maze.saída.should == [0,2]
+    maze = MirrorMaze.new "*.*"
+    maze.entrada.should == [0,1]
+    maze.saída.should == [0,1]
+
+    maze = MirrorMaze.new "***
+                           ...
+                           ***"
+    maze.entrada.should == [1,0]
+    maze.saída.should == [1,2]
   end
+
+
 end

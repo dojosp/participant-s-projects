e8d0fa57a66e444bd308de46d09ff4a67c462e6f
Thiago Rodrigues Colucci <ticolucci@marvin.(none)>
2009-05-11 21:32:35 -0300

    Refatorando e passando para entradas em mazes com centro. Thiago & Ricardo

--- 72-MirrorMazes/lib/MirrorMazes.rb
+++ 72-MirrorMazes/lib/MirrorMazes.rb
@@ -1,12 +1,19 @@
 class MirrorMaze
   attr_reader :entrada, :saida
 
+  def borda? linha, coluna, size
+    (linha == 0 or
+     coluna == 0 or
+     linha == size or
+     coluna == size)
+  end
+
   def initialize mapa
     @mapa = mapa.split
     @mapa.each_with_index do |string, linha|
       linha_array = string.split ""
       linha_array.each_with_index do |caracter, coluna|
-        if caracter == '.'
+        if borda?(linha, coluna, string.size-1) and caracter == '.'
           @entrada = [linha, coluna] if @entrada.nil?
           @saida = [linha, coluna]
         end
--- 72-MirrorMazes/spec/MirrorMazes_spec.rb
+++ 72-MirrorMazes/spec/MirrorMazes_spec.rb
@@ -2,7 +2,7 @@ require 'spec'
 
 require 'lib/MirrorMazes'
 
-describe "MirrorMazes" do
+describe MirrorMaze do
   it "deveria criar um labirinto" do
     maze = MirrorMaze.new "***
                            ...
@@ -10,17 +10,27 @@ describe "MirrorMazes" do
     maze.should_not be_nil
   end
 
-  it "deveria identificar entrada e saída" do
-    maze = MirrorMaze.new "*.*"
-    maze.entrada.should == [0,1]
-    maze.saida.should == [0,1]
+  describe "quando identificando entrada e saida" do
+    it "deveria identificar entrada e saída iguais" do
+      maze = MirrorMaze.new "*.*"
+      maze.entrada.should == [0,1]
+      maze.saida.should == [0,1]
+    end
 
-    maze = MirrorMaze.new "***
-                           ...
-                           ***"
-    maze.entrada.should == [1,0]
-    maze.saida.should == [1,2]
+    it "deveria identificar entrada e saida na mesma linha" do
+      maze = MirrorMaze.new "***
+                             ...
+                             ***"
+      maze.entrada.should == [1,0]
+      maze.saida.should == [1,2]
+    end
+
+    it "deveria identificar entrada fora do centro" do
+      maze = MirrorMaze.new "***
+                             *.*
+                             ...
+                             ***"
+      maze.entrada.should == [2,0]
+    end
   end
-
-
 end

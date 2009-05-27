d9ed6e0c2296f3af77a53966982450daff8b93e1
Dojo <dojo@Nitim-2.local>
2009-05-27 20:18:00 -0300

    Mais refatoracao, Frame agora retorna o resultado (nao mais uma tabela). R e Pedro

--- 73-Lua/specs/bowling_spec.lua
+++ 73-Lua/specs/bowling_spec.lua
@@ -4,23 +4,19 @@ require 'src.bowling'
 describe["Bowling"] = function()
   
   it["should know result of empty frame"] = function()
-  	local f = Frame("-","-")
-  	expect( f:result() ).should_be(0)
+  	expect( Frame("-","-") ).should_be(0)
   end
   
   it["should know result of simple frame"] = function()
-  	local f = Frame("1","-")
-  	expect( f:result() ).should_be(1)
+  	expect( Frame("1","-") ).should_be(1)
   end
   
   it["should know result of simple frame with other value"] = function()
-  	local f = Frame("2","-")
-  	expect( f:result() ).should_be(2)
+  	expect( Frame("2","-") ).should_be(2)
   end
 
   it["should know result of simple frame knocking on second roll"] = function()
-  	local f = Frame("2","3")
-  	expect( f:result() ).should_be(5)
+  	expect( Frame("2","3") ).should_be(5)
   end
 end
 
--- 73-Lua/src/bowling.lua
+++ 73-Lua/src/bowling.lua
@@ -1,14 +1,7 @@
 function Frame(score1, score2)
-  return {
-  		result = function()
-  			return Convert(score1) + Convert(score2)
-  		end
-  	}
+	return Convert(score1) + Convert(score2)
 end
 
 function Convert(score)
-	if score == "-" then
-		return 0
-	end
-	return tonumber(score) 
+	return tonumber(score) or 0 
 end

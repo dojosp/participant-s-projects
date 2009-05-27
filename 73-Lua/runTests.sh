d8a7efa06a7e3be1741598f83ead9d8ff2f48437
Dojo <dojo@Nitim-2.local>
2009-05-27 19:26:08 -0300

    Configuração inicial. Hugo

---
+++ 73-Lua/runTests.sh
@@ -0,0 +1,1 @@
+lua specs/codigo_spec.lua
---
+++ 73-Lua/src/codigo.lua
@@ -0,0 +1,23 @@
+Stack = {}
+ 
+function Stack:new()
+  local o = { stack = {} }
+  self.__index = self
+  setmetatable(o, self)
+  return o
+end
+ 
+function Stack:push(x)
+  self.stack[#self.stack+1] = x
+end
+ 
+function Stack:top()
+  return self.stack[#self.stack]
+end
+ 
+function Stack:pop()
+  if #self.stack == 0 then
+    error("Nothing on the stack")
+  end
+  self.stack[#self.stack] = nil
+end

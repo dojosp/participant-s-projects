817214f50e8603f78298461d0a349fe8d050b095
Dojo <dojo@Nitim-2.local>
2009-06-22 20:46:41 -0300

    Testes para homogeneidade Hugo e Breno

--- 74-BitMaps-Haskell/BitMaps.hs
+++ 74-BitMaps-Haskell/BitMaps.hs
@@ -1,2 +1,8 @@
 module BitMaps where
 
+compacta _ = "0"
+
+homogenea :: String -> Bool
+homogenea "10" = False
+homogenea "01" = False
+homogenea _ = True
--- 74-BitMaps-Haskell/Tests.hs
+++ 74-BitMaps-Haskell/Tests.hs
@@ -5,6 +5,26 @@ import BitMaps
 
 tests = TestList
 	[
+	 testesDeIntegracao,
+	 testesDeHomogeneidade
+	]
+	
+testesDeHomogeneidade = TestList
+	[
+	"Uma matriz de um so 0 deveria ser homogenea" ~:
+	 homogenea "0" ~?= True
+	,"Uma matriz de um so 1 deveria ser homogenea" ~:
+	 homogenea "1" ~?= True
+	,"Uma matriz de com 0 e 1 nao deveria ser homogenea" ~:
+	 homogenea "01" ~?= False
+	,"Uma matriz de com 1 e 0 nao deveria ser homogenea" ~:
+	 homogenea "10" ~?= False
+	]
+
+testesDeIntegracao = TestList
+	[
+	"Uma matriz de um so 0 deveria ser 0" ~:
+	 compacta "0" ~?= "0"
 	]
 
 main = runTestTT tests

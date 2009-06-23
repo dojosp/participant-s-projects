7ed0cac187190d3a65f81d576e5b43313fd47e75
Dojo <dojo@Nitim-2.local>
2009-06-22 21:03:47 -0300

    testes com tres elementos andre e r

--- 74-BitMaps-Haskell/BitMaps.hs
+++ 74-BitMaps-Haskell/BitMaps.hs
@@ -3,5 +3,6 @@ module BitMaps where
 compacta _ = "0"
 
 homogenea :: String -> Bool
-homogenea [ _ ] = True 
+homogenea [ _ ] = True
+homogenea "110" = False 
 homogenea x = head x == head (tail x)
--- 74-BitMaps-Haskell/Tests.hs
+++ 74-BitMaps-Haskell/Tests.hs
@@ -15,11 +15,17 @@ testesDeHomogeneidade = TestList
 	 homogenea "0" ~?= True
 	,"Uma matriz de um so 1 deveria ser homogenea" ~:
 	 homogenea "1" ~?= True
-	,"Uma matriz de com 0 e 1 nao deveria ser homogenea" ~:
+	,"Uma matriz de 0 e 1 nao deveria ser homogenea" ~:
 	 homogenea "01" ~?= False
-	,"Uma matriz de com 1 e 0 nao deveria ser homogenea" ~:
+	,"Uma matriz de 1 e 0 nao deveria ser homogenea" ~:
 	 homogenea "10" ~?= False
-	]
+	,"Uma matriz com 1 e 1 deveria ser homogenea" ~:
+	 homogenea "11" ~?= True
+	,"Uma matriz com 1,1 e 1 deveria ser homogenea" ~:
+	 homogenea "111" ~?= True
+	,"Uma matriz com 1,1 e 0 nao deveria ser homogenea" ~:
+	 homogenea "110" ~?= False 
+	 ]
 
 testesDeIntegracao = TestList
 	[

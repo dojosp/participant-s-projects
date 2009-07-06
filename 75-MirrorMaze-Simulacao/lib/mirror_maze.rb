8b6fb424badfbbfbae27f5344638ddd2cd59a07c
Dojo <dojo@Nitim-2.local>
2009-07-06 20:54:00 -0300

    Primeiro teste ok. Mari e Breno

--- 75-MirrorMaze-Simulacao/lib/mirror_maze.rb
+++ 75-MirrorMaze-Simulacao/lib/mirror_maze.rb
@@ -1,2 +1,8 @@
-class MirrorMaze
+class Labirinto
+	def initialize(mapa)
+	end
+
+	def proximoPonto (partida, direcao)
+		[0,2]
+	end
 end
--- 75-MirrorMaze-Simulacao/spec/mirror_maze_spec.rb
+++ 75-MirrorMaze-Simulacao/spec/mirror_maze_spec.rb
@@ -1,6 +1,24 @@
 require 'spec'
 require 'lib/mirror_maze'
 
-describe MirrorMaze do
-	it "should exists"
+#Percorrer o caminho
+#Andar para frente
+#Virar no espelho
+#Não atravessar a parede
+#Saber se saiu do labirinto
+
+describe Labirinto do
+	it "andar partir de um ponto e uma direcao" do
+		mapa = [["*",".","*"],
+				["*",".", "*"]]
+				
+		partida = [0,1]
+		
+		direcao = "direita"
+		
+		labirinto = Labirinto.new(mapa)
+		labirinto.proximoPonto(partida,direcao).should == [0,2]
+	end
+	
+	
 end

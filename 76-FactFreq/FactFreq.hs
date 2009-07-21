7bab86956ab47ba52b76ed02b4d3f46a03f5fa54
Dojo <dojo@Nitim-2.local>
2009-07-20 21:43:36 -0300

    Passando o contagem recursivo Thiago e Hugo

--- 76-FactFreq/FactFreq.hs
+++ 76-FactFreq/FactFreq.hs
@@ -3,9 +3,8 @@ module FactFreq where
 fatorial 0 = 1
 fatorial n = n * (fatorial $ n-1)
 
-contagem 11 =  [0,2,0,0,0,0,0,0,0,0]
-contagem 10 =  [1,1,0,0,0,0,0,0,0,0]
-contagem n = somaNoDigito n [0,0,0,0,0,0,0,0,0,0]
+contagem n |n >= 10 = somaNoDigito (div n 10) (somaNoDigito (mod n 10) [0,0,0,0,0,0,0,0,0,0])
+           |otherwise = somaNoDigito n [0,0,0,0,0,0,0,0,0,0]
 
 somaNoDigito 0 (x:xs) = x+1 : xs
 somaNoDigito n (x:xs) = x : somaNoDigito (n-1) xs

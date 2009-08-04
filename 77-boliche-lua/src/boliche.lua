ae31189e486cf58daa6cc7561687b2ff45fe0082
Coding Dojo @ SP <dojo@marvin.(none)>
2009-08-03 21:38:10 -0300

    Refatoracao com dois strike seguidos Pedro e Hugo

--- 77-boliche-lua/src/boliche.lua
+++ 77-boliche-lua/src/boliche.lua
@@ -2,19 +2,21 @@ function soma_jogadas(jogadas)
   local soma = 0
 
   for posicao,jogada in pairs(jogadas) do
-    soma = soma + jogada[1]
-   
+    soma = soma + valor_proprio(jogada)
     if eh_strike(jogada) then
-      soma = soma + jogadas[posicao+1][1]
-      if not eh_strike(jogadas[posicao+1]) then
-        soma = soma + jogadas[posicao+1][2]
-      end
-    else
-      soma = soma + jogada[2]
+      soma = soma + valor_proprio(jogadas[posicao+1])
     end
   end
 
   return soma
+end
+
+function valor_proprio(jogada)
+  if jogada[2] == nil then 
+    return jogada[1]
+  else 
+    return jogada[1] + jogada[2]
+  end
 end
 
 function eh_strike(jogada)

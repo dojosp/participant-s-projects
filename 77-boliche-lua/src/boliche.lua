6fa100b4080ed47505141998e434ce76a37f606a
Coding Dojo @ SP <dojo@marvin.(none)>
2009-08-03 21:30:20 -0300

    juntando dois ifs em if e else. Mari e Pedro

--- 77-boliche-lua/src/boliche.lua
+++ 77-boliche-lua/src/boliche.lua
@@ -2,13 +2,14 @@ function soma_jogadas(jogadas)
   local soma = 0
 
   for posicao,jogada in pairs(jogadas) do
+    soma = soma + jogada[1]
+   
     if jogada[1]==10 then    
       soma = soma + jogadas[posicao+1][1] + jogadas[posicao+1][2]
-    end
-    soma = soma + jogada[1]
-    if jogada[2] ~= nil then
+    else
       soma = soma + jogada[2]
     end
   end
+
   return soma
 end

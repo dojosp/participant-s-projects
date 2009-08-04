122c8c8ecdb6a703aafd71a3d95ef53765c1d2f1
Coding Dojo @ SP <dojo@marvin.(none)>
2009-08-03 21:09:43 -0300

    Passando sem roubar para casos sem acertar tudo hugo e Thiago

--- 77-boliche-lua/src/boliche.lua
+++ 77-boliche-lua/src/boliche.lua
@@ -1,5 +1,7 @@
 function soma_jogadas(jogadas) 
-
-  return jogadas[1][1]+jogadas[1][2]+jogadas[2][1]+jogadas[2][2]+jogadas[3][1]+jogadas[3][2]
-
+  local soma = 0
+  for posicao,jogada in pairs(jogadas) do
+    soma=soma + jogada[1]+jogada[2]
+  end
+  return soma
 end

7ab6e6e32578f41823acb58eb387b43d7d43e4f8
Coding Dojo @ SP <dojo@marvin.(none)>
2009-08-03 22:02:43 -0300

    Codigo misterioso que passa tudo Hugo e Mari (no celular)

--- 77-boliche-lua/specs/boliche_spec.lua
+++ 77-boliche-lua/specs/boliche_spec.lua
@@ -3,168 +3,181 @@ require 'src.boliche'
 
 describe["Boliche"] = function()
   it["tudo 0 deve somar 0"] = function()
-  	expect( soma_jogadas({{0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(0)
   end
 
   it["primeira jogada 1 e 1 e resto zero deve ser 2"] = function()
-  	expect( soma_jogadas({{1,1},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({1,1,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(2)
   end
 
 
  it["primeira jogada 1 e 2 e resto zero deve ser 3"] = function()
-  	expect( soma_jogadas({{1,2},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({1,2,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(3)
   end
  it["primeira jogada 2 e 2, e resto zero deve ser 4"] = function()
-  	expect( soma_jogadas({{2,2},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({2,2,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(4)
   end
  it["segunda jogada 2 e 2, e resto zero deve ser 4"] = function()
-  	expect( soma_jogadas({{0,0},
-                          {2,2},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({0,0,
+                          2,2,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(4)
   end
  it["segunda e terceira jogada 2 e 2, e resto zero deve ser 8"] = function()
-  	expect( soma_jogadas({{0,0},
-                          {2,2},
-                          {2,2},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({0,0,
+                          2,2,
+                          2,2,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(8)
   end
 
  it["strike na primeira e 1 na segunda e zero no resto deve dar 12"] = function()
-  	expect( soma_jogadas({{10},
-                          {1,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({10,
+                          1,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(12)
   end
 
  it["strike na primeira e 2 na segunda e zero no resto deve dar 14"] = function()
-  	expect( soma_jogadas({{10},
-                          {2,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({10,
+                          2,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(14)
   end
 
  it["strike na primeira e 2,2 na segunda e zero no resto deve dar 18"] = function()
-  	expect( soma_jogadas({{10},
-                          {2,2},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({10,
+                          2,2,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(18)
   end
 
   it["strike na segunda e 2,2 na terceira e zero no resto deve dar 18"] = function()
-  	expect( soma_jogadas({{0,0},
-                          {10},
-                          {2,2},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({0,0,
+                          10,
+                          2,2,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(18)
   end
 
   it["strike seguido de strike e zero no resto deve dar 30"] = function()
-  	expect( soma_jogadas({{0,0},
-                          {10},
-                          {10},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({0,0,
+                          10,
+                          10,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(30)
   end
   it["tres strikes seguidos e zero no resto deve dar 60"] = function()
-  	expect( soma_jogadas({{0,0},
-                          {10},
-                          {10},
-                          {10},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0},
-                          {0,0}
+  	expect( soma_jogadas({0,0,
+                          10,
+                          10,
+                          10,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0,
+                          0,0
                          }) ).should_be(60)
+  end
+  it["tudo strikes deve dar 300"] = function()
+  	expect( soma_jogadas({10,
+                          10,
+                          10,
+                          10,
+                          10,
+                          10,
+                          10,
+                          10,
+                          10,
+                          10,10,10
+                         }) ).should_be(300)
   end
 end
 
--- 77-boliche-lua/src/boliche.lua
+++ 77-boliche-lua/src/boliche.lua
@@ -1,25 +1,21 @@
 function soma_jogadas(jogadas) 
   local soma = 0
-  if eh_strike(jogadas[4]) then return 60 end
-
+  
   for posicao,jogada in pairs(jogadas) do
-    soma = soma + valor_proprio(jogada)
-    if eh_strike(jogada) then
-      soma = soma + valor_proprio(jogadas[posicao+1])
+    if jogada==10 then
+      if jogadas[posicao+1] ~= nil and jogadas[posicao+2]~=nil then
+        soma = soma + jogadas[posicao+1]
+      end
+
+      if jogadas[posicao+2] ~= nil then
+        soma = soma + jogadas[posicao+2]
+      end
+    end
+
+    if jogadas[posicao+1] ~= nil and jogadas[posicao+2]~=nil then
+      soma = soma + jogada
     end
   end
 
   return soma
-end
-
-function valor_proprio(jogada)
-  if jogada[2] == nil then 
-    return jogada[1]
-  else 
-    return jogada[1] + jogada[2]
-  end
-end
-
-function eh_strike(jogada)
-  return jogada[1] == 10
 end

880edafc0108cafaec69bbfa364c9ec1b60fa3f2
Dojo <dojo@Nitim-2.local>
2009-08-26 18:34:01 -0500

    Refactoring to rollScore Hugo and Pedro

--- 05-KakeAgile2009/Haskell/Bowling.hs
+++ 05-KakeAgile2009/Haskell/Bowling.hs
@@ -5,8 +5,8 @@ import Char
 bowlingScore (frame:frames) = frameScore(frame) + bowlingScore(frames)
 bowlingScore [] = 0
 
-frameScore ['-', '-'] = 0
-frameScore ['-', roll] = (digitToInt roll)
-frameScore [roll, '-'] = (digitToInt roll)
-frameScore [roll1, roll2] = (digitToInt roll1) + (digitToInt roll2)
+frameScore [roll1, roll2] = (rollScore roll1) + (rollScore roll2)
 frameScore _ = 0
+
+rollScore '-' = 0
+rollScore roll = (digitToInt roll)

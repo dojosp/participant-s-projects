0aaeb9f4eb87910c8434d9a8ba3b47132876d459
Dojo <dojo@Nitim-2.local>
2009-08-26 18:31:24 -0500

    Working with minus at first position Hugo and Pedro

--- 05-KakeAgile2009/Haskell/Bowling.hs
+++ 05-KakeAgile2009/Haskell/Bowling.hs
@@ -6,5 +6,6 @@ bowlingScore (frame:frames) = frameScore(frame) + bowlingScore(frames)
 bowlingScore [] = 0
 
 frameScore ['-', '-'] = 0
+frameScore ['-', roll] = (digitToInt roll)
 frameScore [roll1, roll2] = (digitToInt roll1) + (digitToInt roll2)
 frameScore _ = 0
--- 05-KakeAgile2009/Haskell/BowlingTests.hs
+++ 05-KakeAgile2009/Haskell/BowlingTests.hs
@@ -8,17 +8,22 @@ main = runTestTT tests
 tests = TestList
         [testAllZeros,
         testOneSimpleFrame,
-        testAccumulatesFrameScores
+        testAccumulatesFrameScores,
+        testFrameScoreAddsEvenIfFirstNothing
         ]
-
-testAllZeros =
-	"All zeros should score 0" ~:
-	0 ~?= bowlingScore (take 10 (repeat "--"))
+        
+testFrameScoreAddsEvenIfFirstNothing =
+	"frame score should add the second roll even if first is nothing" ~:
+	5 ~?= bowlingScore ("-5" : (take 9 (repeat "--")))
+        
+testAccumulatesFrameScores =
+	"score should accumulates frame scores" ~:
+	5 + 3 ~?= bowlingScore ("23" : ("12" : (take 8 (repeat "--"))))
 
 testOneSimpleFrame =
 	"One simple frame should score the sum of two rolls" ~:
 	5 ~?= bowlingScore ("23" : (take 9 (repeat "--")))
 
-testAccumulatesFrameScores =
-	"score should accumulates frame scores" ~:
-	5 + 3 ~?= bowlingScore ("23" : ("12" : (take 8 (repeat "--"))))
+testAllZeros =
+	"All zeros should score 0" ~:
+	0 ~?= bowlingScore (take 10 (repeat "--"))

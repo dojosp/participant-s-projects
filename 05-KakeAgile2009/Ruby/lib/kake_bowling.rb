3b2fd80a95194fa85c86774d202eed76f7ddb78f
Coding Dojo @ SP <dojo@marvin.(none)>
2009-08-26 19:17:17 -0500

    Refactoring bowling sum for strike Thaigo and Pedro

--- 05-KakeAgile2009/Ruby/lib/kake_bowling.rb
+++ 05-KakeAgile2009/Ruby/lib/kake_bowling.rb
@@ -4,14 +4,19 @@ class KakeBowling
   def self.game pins
     result = 0
     10.times do |frame|
-      roll1 = pins[frame*2]
-      roll2 = pins[frame*2+1]
-      result += roll1 if !roll1.nil?
-      result += roll2 if !roll2.nil?      
-      if roll1 == 10
+      frame_position = frame*2
+      result += KakeBowling.score(pins, frame_position) + KakeBowling.score(pins, frame_position+1)
+      if pins[frame_position] == 10
         result += (pins[(frame+1)*2] + pins[(frame+1)*2+1])
       end
     end
     result
   end
+
+  def self.score(pins, rollIndex)
+    roll = pins[rollIndex]
+    return roll if !roll.nil?
+    return 0      
+  end
 end
+

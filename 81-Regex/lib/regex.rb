532cfca1ceee8c10a0413004f86d156e3a759925
Thiago R. Colucci <thiagorcolucci@agilbits.com.br>
2009-10-27 21:34:09 -0200

    Passando menos roubado para regex ab e string ba
    Mari e Raphael

--- 81-Regex/lib/regex.rb
+++ 81-Regex/lib/regex.rb
@@ -4,12 +4,22 @@ class Regex
   end
 
   def aceita? palavra
-    return true if @regex == palavra
     letras = palavra.split("")
 
+    letras_regex = @regex.split("")
+    primeira = letras_regex[0]
+
     aceita = @regex.empty?
-    letras.each do |letra|
-      aceita ||= letra == @regex
+    letras.each_with_index do |letra, indice|
+      if letra == primeira
+        if(letras_regex.size == 2)
+          segunda = letras_regex[1]
+          aceita ||= letras[indice+1] == segunda
+        else
+          aceita = true
+        end
+        break
+      end
     end
 
     aceita

157126dd4af306d6ab5fa63e00ced5f2d5bc9b1f
Thiago R. Colucci <thiagorcolucci@agilbits.com.br>
2009-10-27 22:13:15 -0200

    refatorando passando slice ao inves do indice Adolfo e Thiago

--- 81-Regex/lib/regex.rb
+++ 81-Regex/lib/regex.rb
@@ -3,23 +3,23 @@ class Regex
     @regex = regex
   end
 
+  def regex_inteira? palavra
+    letras_regex = @regex.split("")
+    letras_batidas = 0
+    while palavra[letras_batidas] == letras_regex[letras_batidas] and
+          letras_batidas < letras_regex.size
+      letras_batidas += 1
+    end
+    letras_batidas == letras_regex.size
+  end
+
   def aceita? palavra
     letras = palavra.split("")
-
-    letras_regex = @regex.split("")
-
     letras.each_with_index do |letra_palavra, indice|
-      letras_batidas = 0
-      while letras[indice+letras_batidas] == letras_regex[letras_batidas] and
-            letras_batidas < letras_regex.size
-        letras_batidas+=1
-      end
-      if letras_batidas == letras_regex.size
-        return true
-      end
+      return true if regex_inteira? letras[indice .. -1]
     end
-
     @regex == palavra
   end
+
 end
 

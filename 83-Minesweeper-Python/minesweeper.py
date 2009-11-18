d10b196630d4b1b5150fe7393c009dd99d08e956
Dojo <dojo@Nitim-2.local>
2009-11-18 15:14:33 -0200

    testes e codigo da sessao inteira.

--- 83-Minesweeper-Python/minesweeper.py
+++ 83-Minesweeper-Python/minesweeper.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python
 
-class Objeto(object):
-	pass
+class CampoMinado(object):
+	def __init__(self, altura, largura):
+		self.matriz = []
+		for i in range (altura):
+			self.matriz.append ([0]*largura)
+
+	def poeUmValido(self, linha, coluna):
+		if(self.posicaoValida(linha, coluna)):
+			self.matriz[linha][coluna] = 1	
+			
+	def adicionaBomba(self, linha, coluna):
+		if(self.posicaoValida(linha, coluna)):
+			self.poeUmValido(linha-1, coluna-1)
+			self.poeUmValido(linha-1, coluna)
+			self.poeUmValido(linha-1, coluna+1)
+			self.poeUmValido(linha, coluna-1)
+			self.matriz[linha][coluna] = '*'
+			self.poeUmValido(linha, coluna+1)
+			self.poeUmValido(linha+1, coluna-1)
+			self.poeUmValido(linha+1, coluna)
+			self.poeUmValido(linha+1, coluna+1)
+
+	def posicaoValida(self, linha, coluna):
+		linhaValida = linha >= 0 and linha < len(self.matriz)
+		colunaValida = coluna >= 0 and coluna < len(self.matriz[0])
+		return linhaValida and colunaValida
--- 83-Minesweeper-Python/minesweeperTest.py
+++ 83-Minesweeper-Python/minesweeperTest.py
@@ -2,10 +2,50 @@
 
 import unittest
 import minesweeper
-from minesweeper import Objeto
+from minesweeper import CampoMinado
 
-class Testes(unittest.TestCase):
-	pass        
-
+class TestesDoCampoMinado(unittest.TestCase):
+	def testeCampoMinadoVazioEhCriadoComTamanho(self):
+		campo = CampoMinado(5, 5)
+		self.assertEquals([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]], campo.matriz)
+		campo = CampoMinado(3, 4)
+		self.assertEquals([[0,0,0,0],[0,0,0,0],[0,0,0,0]], campo.matriz)
+	
+	def testeColocandoBombaNaPosicao0x0DeUmCampo1x1(self):
+		campo = CampoMinado(1, 1)
+		campo.adicionaBomba(0,0)
+		self.assertEquals('*', campo.matriz[0][0])
+		
+	def testeColocandoBombaNaPosicao0x0DeUmCampo1x1ComUmaBomba(self):
+		campo = CampoMinado(1, 1)
+		campo.adicionaBomba(0,0)
+		campo.adicionaBomba(0,0)
+		self.assertEquals('*', campo.matriz[0][0])
+		
+	def testeColocandoBombaNaPosicaoMenos42x42DeUmCampo1x1(self):
+		campo = CampoMinado(1, 1)
+		campo.adicionaBomba(-42,42)
+		self.assertEquals([[0]], campo.matriz)
+	
+	def testeColocandoBombaNaPosicao1x1DeUmCampo1x1(self):
+		campo = CampoMinado(1, 1)
+		campo.adicionaBomba(1,1)
+		self.assertEquals([[0]], campo.matriz)
+		
+	def testeColocandoUmaBombaEmUmCampoMaior(self):
+		campo = CampoMinado(2,3)
+		campo.adicionaBomba(1,2)
+		self.assertEquals([ [0,1, 1 ],
+							[0,1,'*']], campo.matriz)
+		self.assertEquals('*', campo.matriz[1][2])
+	
+	def testeColocandoUmaBombaNoMeioDeUmCampo3x3(self):
+		campo = CampoMinado(3,3)
+		campo.adicionaBomba(1,1)
+		self.assertEquals([ [1, 1, 1 ],
+							[1,'*',1],
+							[1, 1, 1]], campo.matriz)
+		
+		
 if __name__ == '__main__':
     unittest.main()

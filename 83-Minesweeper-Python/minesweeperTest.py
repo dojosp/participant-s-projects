aeac35baf5fd1a2fc17c471869d2598507d4bdcc
Dojo <dojo@Nitim-2.local>
2009-11-18 13:48:50 -0200

    Arquivos base para o dojo vespertino de python. Hugo

---
+++ 83-Minesweeper-Python/minesweeperTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import unittest
+import minesweeper
+from minesweeper import Objeto
+
+class Testes(unittest.TestCase):
+	pass        
+
+if __name__ == '__main__':
+    unittest.main()

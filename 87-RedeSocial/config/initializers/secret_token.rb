f6137cd557f8743305bc0e0fd4ed137b2e0788a5
Fabricio Nascimento <fabriciosn@gmail.com>
2010-09-08 18:11:06 -0300

    Projeto inicial

---
+++ 87-RedeSocial/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedeSocial::Application.config.secret_token = '743ed04605759f6f1bcfc4a651f25b6d98e8c50ed69daa83a689f5b86c339c38df1d222eb934702116f4246c13aacebd4a40fbeed498c112129ced9ad2ffbe3b'

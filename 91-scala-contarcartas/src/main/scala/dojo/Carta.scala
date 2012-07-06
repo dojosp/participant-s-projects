5b2107d9e202968a26fc6050e5de20ecad5c9d92
Jonas Abreu <jonas@vidageek.net>
2012-07-06 00:55:05 -0300

    adicionando o código do Dojo de retorno do IME. O problema foi o Counting Cards http://www.rubyquiz.com/quiz152.html

---
+++ 91-scala-contarcartas/src/main/scala/dojo/Carta.scala
@@ -0,0 +1,17 @@
+package dojo
+
+class Carta(val valor : Int)
+
+object Dois extends Carta(1)
+object Tres extends Carta(1)
+object Quatro extends Carta(1)
+object Cinco extends Carta(1)
+object Seis extends Carta(1)
+object Sete extends Carta(1)
+object Oito extends Carta(0)
+object Nove extends Carta(0)
+object Dez extends Carta(-1)
+object J extends Carta(-1)
+object Q extends Carta(-1)
+object K extends Carta(-1)
+object A extends Carta(-1)
